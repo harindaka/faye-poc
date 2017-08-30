9ff387ff0fc0e9e1be7ae7a16029f84d199553e7
Hemal Harindaka <hathulathmudali@gmail.com>
2017-08-31 00:56:38 +0530

    fixed nginx ssl configuration

--- server/src/index.js
+++ server/src/index.js
@@ -39,83 +39,83 @@
     let OutgoingMessageInterceptor = require('./outgoing-message-interceptor');
     let outgoingMessageInterceptor = new OutgoingMessageInterceptor();
 
-    // fayeServer.addExtension({
-    //     incoming: function(message, callback) {         
-    //         let parsedRequest = channelRequestParser.parseRequest(message);
-    //         if(parsedRequest.requestType === 'subscription'){
-    //             subscriptionInterceptor.processRequest(parsedRequest).then(() => {
-    //                 callback(message);
-    //             }).catch((error) => {
-    //                 console.log('[' + parsedRequest.channel + '] Rejecting subscription due to error: ' + errorUtil.toMessage(error));
-    //                 message.error = errorUtil.toJson(error);
-    //                 callback(message);
-    //             });
-    //         }
-    //         else if(parsedRequest.requestType === 'meta'){
-    //             callback(message);
-    //         }
-    //         else{ //parsedRequest.requestType === 'message'
-    //             incomingMessageInterceptor.processRequest(parsedRequest, message).then(() => {
-    //                 callback(message);
-    //             }).catch((error) => {
-    //                 console.log('[' + parsedRequest.channel + '] Rejecting message due to error: ' + errorUtil.toMessage(error));
-    //                 message.error = errorUtil.toJson(error);
-    //                 callback(message);
-    //             });
-    //         }            
-    //     },
+    fayeServer.addExtension({
+        incoming: function(message, callback) {         
+            let parsedRequest = channelRequestParser.parseRequest(message);
+            if(parsedRequest.requestType === 'subscription'){
+                subscriptionInterceptor.processRequest(parsedRequest).then(() => {
+                    callback(message);
+                }).catch((error) => {
+                    console.log('[' + parsedRequest.channel + '] Rejecting subscription due to error: ' + errorUtil.toMessage(error));
+                    message.error = errorUtil.toJson(error);
+                    callback(message);
+                });
+            }
+            else if(parsedRequest.requestType === 'meta'){
+                callback(message);
+            }
+            else{ //parsedRequest.requestType === 'message'
+                incomingMessageInterceptor.processRequest(parsedRequest, message).then(() => {
+                    callback(message);
+                }).catch((error) => {
+                    console.log('[' + parsedRequest.channel + '] Rejecting message due to error: ' + errorUtil.toMessage(error));
+                    message.error = errorUtil.toJson(error);
+                    callback(message);
+                });
+            }            
+        },
 
-    //     outgoing: function(message, callback){
-    //         outgoingMessageInterceptor.processResponse(message).then(() => {
-    //             callback(message);
-    //         }).catch((error) => {
-    //             console.log('[' + parsedRequest.channel + '] Failed to process outgoing message due to error: ' + errorUtil.toMessage(error));
-    //             message.error = errorUtil.toJson(error);
-    //             callback(message);
-    //         });
-    //     }
-    // });
+        outgoing: function(message, callback){
+            outgoingMessageInterceptor.processResponse(message).then(() => {
+                callback(message);
+            }).catch((error) => {
+                console.log('[' + parsedRequest.channel + '] Failed to process outgoing message due to error: ' + errorUtil.toMessage(error));
+                message.error = errorUtil.toJson(error);
+                callback(message);
+            });
+        }
+    });
 
-    // fayeServer.on('subscribe', function(clientId, channel) {        
-    //     let nickname = channelRequestParser.parseNicknamefromUserChannelUrl(channel);
-    //     if(nickname !== null){                        
-    //         let tokenMessage = messageFactory.create('auth-token');
-    //         let userJoinMessage = messageFactory.create('chat');
-    //         userJoinMessage.text = nickname + ' joined the chat';
+    fayeServer.on('subscribe', function(clientId, channel) {        
+        let nickname = channelRequestParser.parseNicknamefromUserChannelUrl(channel);
+        if(nickname !== null){                        
+            let tokenMessage = messageFactory.create('auth-token');
+            let userJoinMessage = messageFactory.create('chat');
+            userJoinMessage.text = nickname + ' joined the chat';
 
-    //         sessionStore.retrieveToken(nickname, clientId).then((authToken) => {
-    //             tokenMessage.authToken = authToken;
-    //             tokenMessage.clientId = clientId;
-    //             return serversideClient.publish(channel, tokenMessage);
-    //         }).then(() => {
-    //             console.log('[' + nickname + '][' + channel + '] Published auth-token');                
-    //         }).catch((error) => {
-    //             console.log('[' + nickname + '][' + channel + '] Failed to publish auth-token ' + JSON.stringify(tokenMessage) + ' due to error: ' + errorUtil.toMessage(error));                
-    //         }).then(() => {
-    //             return serversideClient.publish(config.server.faye.topics.chat.url, userJoinMessage);
-    //         }).then(() => {
-    //             console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Published message: ' + JSON.stringify(userJoinMessage));                
-    //         }).catch((error) => {
-    //             console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Failed to publish message ' + JSON.stringify(userJoinMessage) + ' due to error: ' + errorUtil.toMessage(error));                
-    //         });                                   
-    //     }
-    // });
+            sessionStore.retrieveToken(nickname, clientId).then((authToken) => {
+                tokenMessage.authToken = authToken;
+                tokenMessage.clientId = clientId;
+                return serversideClient.publish(channel, tokenMessage);
+            }).then(() => {
+                console.log('[' + nickname + '][' + channel + '] Published auth-token');                
+            }).catch((error) => {
+                console.log('[' + nickname + '][' + channel + '] Failed to publish auth-token ' + JSON.stringify(tokenMessage) + ' due to error: ' + errorUtil.toMessage(error));                
+            }).then(() => {
+                return serversideClient.publish(config.server.faye.topics.chat.url, userJoinMessage);
+            }).then(() => {
+                console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Published message: ' + JSON.stringify(userJoinMessage));                
+            }).catch((error) => {
+                console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Failed to publish message ' + JSON.stringify(userJoinMessage) + ' due to error: ' + errorUtil.toMessage(error));                
+            });                                   
+        }
+    });
 
-    // fayeServer.on('unsubscribe', function(clientId, channel) {
-    //     let nickname = channelRequestParser.parseNicknamefromUserChannelUrl(channel);
-    //     if(nickname !== null){                        
-    //         let userLeaveMessage = messageFactory.create('chat');
-    //         userLeaveMessage.text = nickname + ' left the chat';
+    fayeServer.on('unsubscribe', function(clientId, channel) {
+        let nickname = channelRequestParser.parseNicknamefromUserChannelUrl(channel);
+        if(nickname !== null){                        
+            let userLeaveMessage = messageFactory.create('chat');
+            userLeaveMessage.text = nickname + ' left the chat';
 
-    //         sessionStore.deleteToken(nickname, clientId).then(() => {
-    //             return serversideClient.publish(config.server.faye.topics.chat.url, userLeaveMessage);
-    //         }).then(() => {
-    //             console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Published chat: ' + JSON.stringify(userLeaveMessage));
-    //         }).catch((error) => {
-    //             console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Failed to publish chat ' + JSON.stringify(userLeaveMessage) + ' due to error: ' + errorUtil.toMessage(error));                
-    //         });                   
-    //     }
-    // });
+            sessionStore.deleteToken(nickname, clientId).then(() => {
+                return serversideClient.publish(config.server.faye.topics.chat.url, userLeaveMessage);
+            }).then(() => {
+                console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Published chat: ' + JSON.stringify(userLeaveMessage));
+            }).catch((error) => {
+                console.log('[' + nickname + '][' + config.server.faye.topics.chat.url + '] Failed to publish chat ' + JSON.stringify(userLeaveMessage) + ' due to error: ' + errorUtil.toMessage(error));                
+            });                   
+        }
+    });
 
     function createTopicIndex(){
         let index = {};
@@ -136,22 +136,22 @@
         else{
             console.log('Successfully initialized server on port ' + config.server.port);
 
-            let publishedMessageCount = 0;
-            setInterval(() => {          
-                let topicUrl = config.server.faye.topics.log.url;  
-                serversideClient.publish(topicUrl, { 
-                    text: 'Hello!', 
-                    clientId: null,
-                    publishedMessageCount: publishedMessageCount
-                }, {
-                    deadline: 10, //client will not attempt to resend the message any later than 10 seconds after your first publish() call
-                    attempts: 3 //how many times the client will try to send a message before giving up, including the first attempt
-                }).then(() => {
-                    publishedMessageCount += 1;                    
-                }).catch((error) => {
-                    console.log('The server explicitly rejected publishing the message due to error: ' + errorUtil.toMessage(error));
-                });
-            }, 3000);
+            // let publishedMessageCount = 0;
+            // setInterval(() => {          
+            //     let topicUrl = config.server.faye.topics.log.url;  
+            //     serversideClient.publish(topicUrl, { 
+            //         text: 'Hello!', 
+            //         clientId: null,
+            //         publishedMessageCount: publishedMessageCount
+            //     }, {
+            //         deadline: 10, //client will not attempt to resend the message any later than 10 seconds after your first publish() call
+            //         attempts: 3 //how many times the client will try to send a message before giving up, including the first attempt
+            //     }).then(() => {
+            //         publishedMessageCount += 1;                    
+            //     }).catch((error) => {
+            //         console.log('The server explicitly rejected publishing the message due to error: ' + errorUtil.toMessage(error));
+            //     });
+            // }, 3000);
         }
     });
 })();
